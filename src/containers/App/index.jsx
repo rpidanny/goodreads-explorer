cadff6b69fae996ca25f976f11e751ddd03db943
rpidanny <abhishekmaharjan1993@gmail.com>
2019-01-07 23:01:21 +0100

    Update project boilerplate

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from '../../assets/images/logo.svg'
+import './style.css'
 
 class App extends Component {
   render () {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/containers/App.js</code> and save to reload.
           </p>
           <a
             className='App-link'
