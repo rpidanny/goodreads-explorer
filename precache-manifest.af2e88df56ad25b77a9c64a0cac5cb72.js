9721b3fc14c079697de5582ce6a984b8d4177660
rpidanny (from Travis CI) <abhishekmaharjan1993@gmail.com>
2019-01-08 20:46:42 +0000

    Deploy rpidanny/goodreads-explorer to github.com/rpidanny/goodreads-explorer.git:gh-pages

---
+++ precache-manifest.af2e88df56ad25b77a9c64a0cac5cb72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a9c536dfebf0339e854",
+    "url": "/static/css/main.e3fc99bd.chunk.css"
+  },
+  {
+    "revision": "3a9c536dfebf0339e854",
+    "url": "/static/js/main.3a9c536d.chunk.js"
+  },
+  {
+    "revision": "973a213e3f2889b143e1",
+    "url": "/static/js/1.973a213e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7af9561e161ea49a9bc07f3c7a3363a2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/goodreads-explorer/precache-manifest.3f9eaa8683f133c435b7fdb8d7057479.js"
+  "/precache-manifest.af2e88df56ad25b77a9c64a0cac5cb72.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/goodreads-explorer/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
