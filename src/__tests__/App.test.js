0cf758818a05f0b181d067d2cd7dff72f7b1dc26
rpidanny <abhishekmaharjan1993@gmail.com>
2019-01-08 21:18:23 +0100

    Update test script

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,16 @@
+/* global it */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import configureStore from '../configureStore'
 import App from '../containers/App'
+
+const store = configureStore()
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import configureStore from './configureStore'
 import App from './containers/App'
