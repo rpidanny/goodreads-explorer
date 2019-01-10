93cfc4907803d6ff685345fc3bf5c8abb2350c2c
rpidanny <abhishekmaharjan1993@gmail.com>
2019-01-10 22:06:06 +0100

    Replace Home component class with function

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 // antd components
 import Row from 'antd/es/row'
@@ -11,35 +11,31 @@ import './style.css'
 
 import goodReadsLogo from '../../assets/images/goodreads-logo-transparent.png'
 
-class Home extends Component {
-  render () {
-    return (
-      <Layout className='Home' >
-        <Row className='center'>
-          <Col span={6} />
-          <Col span={12}>
-            <img
-              src={goodReadsLogo}
-              width='100%'
-              alt='Goodreads'
-            />
-          </Col>
-          <Col span={6} />
-        </Row>
-        <Row className='center'>
-          <Col span={8} />
-          <Col span={8}>
-            <Search
-              placeholder='Enter user id'
-              onSearch={this.props.searchHandler}
-              enterButton={this.props.searchHandler}
-            />
-          </Col>
-          <Col span={8} />
-        </Row>
-      </Layout>
-    )
-  }
-}
+const Home = (props) => (
+  <Layout className='Home' >
+    <Row className='center'>
+      <Col span={6} />
+      <Col span={12}>
+        <img
+          src={goodReadsLogo}
+          width='100%'
+          alt='Goodreads'
+        />
+      </Col>
+      <Col span={6} />
+    </Row>
+    <Row className='center'>
+      <Col span={8} />
+      <Col span={8}>
+        <Search
+          placeholder='Enter user id'
+          onSearch={props.searchHandler}
+          enterButton={props.searchHandler}
+        />
+      </Col>
+      <Col span={8} />
+    </Row>
+  </Layout>
+)
 
 export default Home
