af4810aa5e8fe90d6d19a7c074ab5d7a7d4afb16
rpidanny <abhishekmaharjan1993@gmail.com>
2025-12-12 11:11:05 +0100

    chore: update build

---
+++ precache-manifest.44fd2ff312c7b797a945c63419c2eb05.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b24bba400a3243ee0699ab3f3e7a74",
+    "url": "/goodreads-explorer/index.html"
+  },
+  {
+    "revision": "dba11edb29be0f676635",
+    "url": "/goodreads-explorer/static/css/13.1898fd6a.chunk.css"
+  },
+  {
+    "revision": "2566ba60df5f82f56549",
+    "url": "/goodreads-explorer/static/css/14.b1112a19.chunk.css"
+  },
+  {
+    "revision": "28dd0e80a2ea6c32a973",
+    "url": "/goodreads-explorer/static/css/15.9794abb5.chunk.css"
+  },
+  {
+    "revision": "55a6dec005718fcd4f37",
+    "url": "/goodreads-explorer/static/css/16.deee88ff.chunk.css"
+  },
+  {
+    "revision": "3c6f3c4892733cc61037",
+    "url": "/goodreads-explorer/static/css/17.3bb60984.chunk.css"
+  },
+  {
+    "revision": "3b2fcb5ff7f198a7d797",
+    "url": "/goodreads-explorer/static/css/18.dc2297b4.chunk.css"
+  },
+  {
+    "revision": "b51e178d067594129c1c",
+    "url": "/goodreads-explorer/static/css/9.91e37632.chunk.css"
+  },
+  {
+    "revision": "21171566ff59c5850bbc",
+    "url": "/goodreads-explorer/static/css/main.58b30286.chunk.css"
+  },
+  {
+    "revision": "30df6ee6bd7b6406eeb2",
+    "url": "/goodreads-explorer/static/js/0.fa172378.chunk.js"
+  },
+  {
+    "revision": "66b3f93729414a8156e5",
+    "url": "/goodreads-explorer/static/js/1.017c01a3.chunk.js"
+  },
+  {
+    "revision": "ba9f334d074b3700def6",
+    "url": "/goodreads-explorer/static/js/10.8548a3f3.chunk.js"
+  },
+  {
+    "revision": "2c6a4320bca9ecbb906c",
+    "url": "/goodreads-explorer/static/js/11.a138963c.chunk.js"
+  },
+  {
+    "revision": "077b30c9a4471b3ac417",
+    "url": "/goodreads-explorer/static/js/12.74f18a99.chunk.js"
+  },
+  {
+    "revision": "dba11edb29be0f676635",
+    "url": "/goodreads-explorer/static/js/13.fe8ec07d.chunk.js"
+  },
+  {
+    "revision": "2566ba60df5f82f56549",
+    "url": "/goodreads-explorer/static/js/14.8d9f3697.chunk.js"
+  },
+  {
+    "revision": "28dd0e80a2ea6c32a973",
+    "url": "/goodreads-explorer/static/js/15.a505834b.chunk.js"
+  },
+  {
+    "revision": "55a6dec005718fcd4f37",
+    "url": "/goodreads-explorer/static/js/16.2f473e61.chunk.js"
+  },
+  {
+    "revision": "3c6f3c4892733cc61037",
+    "url": "/goodreads-explorer/static/js/17.1004c38f.chunk.js"
+  },
+  {
+    "revision": "3b2fcb5ff7f198a7d797",
+    "url": "/goodreads-explorer/static/js/18.0b7550ea.chunk.js"
+  },
+  {
+    "revision": "e1e65427e1c5a7d03399",
+    "url": "/goodreads-explorer/static/js/19.40f8abc9.chunk.js"
+  },
+  {
+    "revision": "8355b0e46e4df1869ffa",
+    "url": "/goodreads-explorer/static/js/2.9df0c826.chunk.js"
+  },
+  {
+    "revision": "5043b7dee26f255f27ef",
+    "url": "/goodreads-explorer/static/js/20.2fb49e5a.chunk.js"
+  },
+  {
+    "revision": "34684dddca07693cc11e",
+    "url": "/goodreads-explorer/static/js/21.41601757.chunk.js"
+  },
+  {
+    "revision": "64e5fd3872fb43daefd5",
+    "url": "/goodreads-explorer/static/js/22.f05be457.chunk.js"
+  },
+  {
+    "revision": "483c82004281d10221ab",
+    "url": "/goodreads-explorer/static/js/3.92dc3d7a.chunk.js"
+  },
+  {
+    "revision": "852b16305d3cfaf93493",
+    "url": "/goodreads-explorer/static/js/4.794c81ed.chunk.js"
+  },
+  {
+    "revision": "82dd2e8ce35bee4ec942",
+    "url": "/goodreads-explorer/static/js/5.c284794f.chunk.js"
+  },
+  {
+    "revision": "f29661de95af124fa316",
+    "url": "/goodreads-explorer/static/js/8.b12dc3ba.chunk.js"
+  },
+  {
+    "revision": "b51e178d067594129c1c",
+    "url": "/goodreads-explorer/static/js/9.1d170e6c.chunk.js"
+  },
+  {
+    "revision": "a8ec1291127c214a3b47eecd791ea4ce",
+    "url": "/goodreads-explorer/static/js/9.1d170e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21171566ff59c5850bbc",
+    "url": "/goodreads-explorer/static/js/main.9021e8ed.chunk.js"
+  },
+  {
+    "revision": "d06ef509880e57ead643",
+    "url": "/goodreads-explorer/static/js/runtime-main.9cc09719.js"
+  },
+  {
+    "revision": "eff85db6aaed5480f0fa691e47cdede8",
+    "url": "/goodreads-explorer/static/media/good_reads_explorer.eff85db6.png"
+  }
+]);
